658a5265bfde109da00672c5eace72f963dbd0fa
Sean Lossef <sean.lossef@keycaptureenergy.com>
2022-01-27 15:20:03 -0500

    Override _create_index_name, add imports

--- mssql/schema.py
+++ mssql/schema.py
@@ -17,7 +17,7 @@ from django.db.backends.ddl_references import (
     Statement as DjStatement,
     Table,
 )
-from django.db.backends.utils import split_identifier
+from django.db.backends.utils import names_digest, split_identifier
 from django import VERSION as django_version
 from django.db.models import Index, UniqueConstraint
 from django.db.models.fields import AutoField, BigAutoField
@@ -878,6 +878,34 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         )
     
     # CUSTOM OVERRIDES BELONGING TO FORK
+
+    def _create_index_name(self, table_name, column_names, suffix=""):
+        """
+        Generate a unique name for an index/unique constraint.
+        The name is divided into 3 parts: the table name, the column names,
+        and a unique digest and suffix.
+        """
+        _, table_name = split_identifier(table_name.split('[')[-1])
+        hash_suffix_part = '%s%s' % (names_digest(table_name, *column_names, length=8), suffix)
+        max_length = self.connection.ops.max_name_length() or 200
+        # If everything fits into max_length, use that name.
+        index_name = '%s_%s_%s' % (table_name, '_'.join(column_names), hash_suffix_part)
+        if len(index_name) <= max_length:
+            return index_name
+        # Shorten a long suffix.
+        if len(hash_suffix_part) > max_length / 3:
+            hash_suffix_part = hash_suffix_part[:max_length // 3]
+        other_length = (max_length - len(hash_suffix_part)) // 2 - 1
+        index_name = '%s_%s_%s' % (
+            table_name[:other_length],
+            '_'.join(column_names)[:other_length],
+            hash_suffix_part,
+        )
+        # Prepend D if needed to prevent the name from starting with an
+        # underscore or a number (not permitted on Oracle).
+        if index_name[0] == "_" or index_name[0].isdigit():
+            index_name = "D%s" % index_name[:-1]
+        return index_name
 
     def _fk_constraint_name(self, model, field, suffix):
         def create_fk_name(*args, **kwargs):
